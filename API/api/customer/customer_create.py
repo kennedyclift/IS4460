2a6c3099a3f9edeea37ae1ad46c05a2d0a3cbf15
Kennedy Clift <u1286164@utah.edu>
2024-03-21 15:45:00 -0600

    March 21

---
+++ API/api/customer/customer_create.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+api_url = 'http://localhost:8000/api/customers/'
+
+
+customer_data = {
+    "name": "Customer X",
+    "email": "customerX@example.com",
+    "phone_number": "8014445555"
+}
+
+response = requests.post(api_url, data=json.dumps(customer_data), headers={'Content-Type': 'application/json'}, verify=False)
+
+if response.status_code == 201:
+    print("Customers created successfully.")
+else:
+    print("Error creating customers.")
+
