d286eb39e9e8b6c95093b25ad19665cbf92bb6ee
Kennedy Clift <u1286164@utah.edu>
2024-03-25 13:36:38 -0600

    March 25

--- security_proj/security_proj/urls.py
+++ security_proj/security_proj/urls.py
@@ -21,5 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('customer/',include('customer.urls')),
-    path('accounts/', include('django.contribu.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
