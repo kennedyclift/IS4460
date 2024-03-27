56722aba1e38cfcaaa74ec9ee1d164c8cbe9f234
Kennedy Clift <u1286164@utah.edu>
2024-03-27 13:17:28 -0600

    March 27

--- security_proj/customer/views.py
+++ security_proj/customer/views.py
@@ -2,13 +2,36 @@ from django.shortcuts import render
 from django.views import View
 from .models import Customer
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .serializers import CustomerSerializer
+from rest_framework import generics
+
+
+def can_view_cust(request):
+    return request.user.groups.filter(name='Can View Customers').exists()
+
+class CustomerListCreateView(generics.ListCreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+class CustomerDetailView(generics.RetrieveUpdateDestoryAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
 
 
 class CustomerListView(LoginRequiredMixin, View):
 
     def get(self,request):
-        customers = Customer.objects.all()
-        return render(request=request,
-                      template_name='customer/customer_list.html',
-                      context = {'customers':customers}
-                      )
+
+        user_message = ""
+        customers = None
+
+        if not can_view_cust(request):
+            user_message = "Cannot View Customers. Not in 'Can View Customers' Group!"
+        
+        else:
+            customers = Customer.objects.all()
+
+        return render(request = request,
+                      template_name = 'customer/customer_list.html',
+                      context = {'customers':customers, 'user_message':user_message})
