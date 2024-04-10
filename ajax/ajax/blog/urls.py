754bb1cba561a6d02bdbc9319059a8b943e2e9db
Kennedy Clift <u1286164@utah.edu>
2024-04-10 12:32:22 -0600

    April 10

--- ajax/ajax/blog/urls.py
+++ ajax/ajax/blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 from blog import views
 
 urlpatterns = [
+    path('like_post/<int:post_id>/', views.like_post, name='like_post'),
     path("list/", views.get_posts,name="get_posts")
 ]
